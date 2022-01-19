7fd50261212cf61f0f5cec7709e5cd19468bc644
yikezawa <y.ikezawa93@gmail.com>
2022-01-19 22:22:50 +0900

    mod

---
+++ src/Complex.js
@@ -0,0 +1,13 @@
+class Complex {
+  constructor(real, imaginary) {
+    this.r = real;
+    this.i = imaginary;
+  }
+
+  plus(that) {
+    return new Complex(this.r + that.r, this.i + that.i);
+  }
+  times(that) {
+    return new Complex()
+  }
+}
