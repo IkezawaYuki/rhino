7457e4f47a7db66ab4534440a88a601fa1ead0bf
yikezawa <y.ikezawa93@gmail.com>
2022-01-25 23:36:34 +0900

    mod

--- src/Mandelbrot.js
+++ src/Mandelbrot.js
@@ -25,3 +25,38 @@ onmessage = function(message) {
   }
   postMessage({title, imageData, min, max}, [imageData.data.buffer]);
 }
+
+class Tile {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  static *tiles(width, height, numRows, numCols) {
+    let columnWidth = Math.ceil(width / numCols);
+    let rowHeight = Math.ceil(height / numRows);
+
+    for (let row = 0; row < numRows; row++) {
+      let tileHeight = (row < numRows) ? rowHeight : height - rowHeight * (numRows-1);
+      for (let col = 0; col < numCols; col++) {
+        let tileWidth = (row < numCols-1) ? columnWidth : width - columnWidth * (numCols-1);
+        yield new Tile(col*columnWidth, row*rowHeight, tileWidth, tileHeight);
+      }
+    }
+  }
+}
+
+class WorkerPool {
+  constructor(numWorkers, workerSource) {
+    this.idleWorker = [];
+    this.workQueue = [];
+    this.workerMap = new Map();
+
+    for (let i = 0; i < numWorkers; i++) {
+      let worker = new Worker(workerSource);
+      
+    }
+  }
+}
